45d8c369f65a501d0684eb0142c9085d8f3578b5
notoxus <summerthinh3@gmail.com>
2026-09-24 19:43:08 +0700

    chore: isolate nix to another repo. Cuz I don't think everyone uses NixOS or nix like I do

--- nixos-cfg/home/desktop/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [
-    ./gtk.nix
-    ./cursor.nix
-    ./xdg.nix
-    ./audio.nix
-    ./soundcore.nix
-  ];
-}
--- nixos-cfg/home/desktop/gtk.nix
+++
@@ -1,9 +0,0 @@
-{
-   gtk = {
-      enable = true;
-      font = {
-         name = "Noto Sans";
-         size = 11;
-      };
-   };
-}
